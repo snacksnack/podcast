0a93ba304556c6d8dfedaf6a27072c11671e60d2
Reid Collins <hihelloreid@gmail.com>
2016-08-15 07:21:05 -0400

    create episode model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   devise_for :podcasts
-  resources :podcasts, only: [:index, :show]
+  resources :podcasts, only: [:index, :show] do
+    resources: :episodes
+  end
   root 'welcome#index'
 
 end
