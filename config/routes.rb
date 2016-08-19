3485beee557dadb4ef31762046360e058b98a6b9
Reid Collins <hihelloreid@gmail.com>
2016-08-18 22:03:55 -0400

    add episode controller and views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
   devise_for :podcasts
-  resources :podcasts, only: [:index, :show]
+  
   root 'welcome#index'
 
+  resources :podcasts, only: [:index, :show] do
+    resources :episodes
+  end
 end
