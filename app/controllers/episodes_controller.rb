64dc6d4ed46d26fc27ec06e2cafa0cf58145255d
Reid Collins <hihelloreid@gmail.com>
2016-08-19 20:53:32 -0400

    add authorization to podcast

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+  before_action :authenticate_podcast!, except: [:show]
+  before_filter :require_permission
   before_action :find_podcast
   before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
@@ -48,5 +50,12 @@ class EpisodesController < ApplicationController
 
   def episode_params
     params.require(:episode).permit(:title,:description)
+  end
+
+  def require_permission
+    podcast = find_podcast
+    if current_podcast != podcast
+      redirect_to root_path, notice: "Sorry, authorization error."
+    end
   end
 end
